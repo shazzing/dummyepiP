530d9c7f4590407bae3727f6ff3a133cc46512b5
shazzing <kewlfundas@gmail.com>
2016-07-29 06:21:54 -0400

    Initial Commit

---
+++ epi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  epi
+//
+//  Created by shashank hegde on 7/29/16.
+//  Copyright © 2016 shashank hegde. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

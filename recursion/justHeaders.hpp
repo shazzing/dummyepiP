8c95d3c0952ed838915c39f974d1e5879de475a0
shazzing <kewlfundas@gmail.com>
2016-08-28 18:52:14 -0400

    add a header to contain all std includes.

---
+++ recursion/justHeaders.hpp
@@ -0,0 +1,21 @@
+//
+//  justHeaders.hpp
+//  epi
+//
+//  Created by shashank hegde on 8/28/16.
+//  Copyright © 2016 shashank hegde. All rights reserved.
+//
+
+#ifndef justHeaders_h
+#define justHeaders_h
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+#endif /* justHeaders_h */
